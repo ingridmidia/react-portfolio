7ff47fe8df8c9d3bdaeb967740599247227419ce
ingridmidia <ingridmidia@gmail.com>
2023-11-24 16:12:08 -0600

    implemented header

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,27 @@
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <strong>Ingrid Miranda</strong>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="#">About Me</a>
+        </li>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">Resume</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
