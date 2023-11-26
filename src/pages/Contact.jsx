d5a15468ea0bdf95286b9ebf3d4bb48a7c7971a8
ingridmidia <ingridmidia@gmail.com>
2023-11-26 15:29:21 -0600

    add validation to contact fields

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,29 @@
+import "../styles/Contact.css";
+
+import { useState } from "react";
+
 function Contact() {
+  const [name, setName] = useState(false);
+  const [email, setEmail] = useState(false);
+  const [message, setMessage] = useState(false);
+
   return (
     <form>
       <label htmlFor="name">
         Name
-        <input type="text" id="name" name="name" placeholder="Name" required />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          required
+          onBlur={(e) => {
+            setName(e.target.value.trim() === "");
+          }}
+        />
+        <p className={name ? "error-message-visible" : "error-message-hidden"}>
+          You must enter a name
+        </p>
       </label>
       <label htmlFor="email">
         Email
@@ -13,7 +33,14 @@ function Contact() {
           name="email"
           placeholder="Email"
           required
+          onBlur={(e) => {
+              const emailRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+              setEmail(!emailRegex.test(e.target.value.trim()));
+          }}
         />
+        <p className={email ? "error-message-visible" : "error-message-hidden"}>
+          You must enter a valid email
+        </p>
       </label>
       <label htmlFor="message">
         Message
@@ -23,7 +50,15 @@ function Contact() {
           name="message"
           placeholder="Your message"
           required
+          onBlur={(e) => {
+            setMessage(e.target.value.trim() === "");
+          }}
         />
+        <p
+          className={message ? "error-message-visible" : "error-message-hidden"}
+        >
+          You must enter a message
+        </p>
       </label>
       <button type="submit">Send</button>
     </form>
